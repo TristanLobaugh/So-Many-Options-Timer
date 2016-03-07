41dc7360edffd157a2794bcb640350006c281bf4
TristanLobaugh <ttrader_atl@yahoo.com>
2016-03-06 20:34:06 -0500

    Added many features
    Added the countdown clock with 3 separate option that will change the
    background once chosen and start the countdown via native javascript.
    Added reset and pause buttons that also work via javascript. Finally
    changed background and cleared field once the countdown ends.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
-
+var endTimeStamp;
+// var endTime = new Date(2016,2,5,17,0,0); will get this from the individual timeTill variables
+// var endTimeStamp = Date.parse(endTime);
+var timer = document.getElementById("countdown-wrapper");
+var weeks = document.getElementById("weeks");
+var days = document.getElementById("days");
+var hours = document.getElementById("hours");
+var minutes = document.getElementById("minutes");
+var seconds = document.getElementById("seconds");
+var countItDown;
 
 // toggles the "Choose an Option Button" options to show and display
 function myOptions()	{
@@ -28,8 +37,8 @@ function subtract(id)	{
 	value--;
 	document.getElementById(id).innerHTML = value;
 }
-
-function reset()	{
+// Gets the current time plus secsonds and assigns it to the time-picker.
+function initTime()	{
 	var now = Date.now();
 	var plusMin = now + (60*1000); // 1 minutes in milliseconds
 	var newDate = new Date(plusMin);
@@ -48,6 +57,106 @@ function reset()	{
 	second.innerHTML = sec;
 }
 
+function getPickedDate()	{
+	var year = document.getElementById("year").innerHTML;
+	var month = (document.getElementById("month").innerHTML) - 1;
+	var day = document.getElementById("day").innerHTML;
+	var hour = document.getElementById("hour").innerHTML;
+	var minute = document.getElementById("minute").innerHTML;
+	var second = document.getElementById("second").innerHTML;
 
-reset();
+	var dateChosen = new Date(year, month, day, hour, minute, second);
+	var timeLeft = dateChosen - Date.now();
+	console.log(timeLeft);
+
+	return timeLeft;
+}
+
+
+function clearTop()	{
+	document.getElementById("intro").style.display = "none";	
+	document.getElementById("time-picker-wrapper").style.display = "none";
+	document.getElementById("drop-down").style.display = "none";
+	document.getElementById("countdown-timer").style.display = "block";
+	document.getElementById("pause-button").style.display = "block";
+
+}
+
+function transitionTrap()	{
+	var timeTillTrap = getPickedDate();
+	clearTop();
+	document.getElementById("trap-header").style.display = "block";
+	document.body.style.backgroundImage = "url(./img/death_star.jpg";
+	endTimeStamp = timeTillTrap;
+	countItDown = setInterval( function() {countDownTimer(); }, 1000);
+}
+
+function transitionTime()	{
+	var timeTillLighting = getPickedDate();
+	clearTop();
+	document.getElementById("lighting-header").style.display = "block";
+	document.body.style.backgroundImage = "url(./img/backtothefuture.jpg";
+	endTimeStamp = timeTillLighting;
+	countItDown = setInterval( function() {countDownTimer(); }, 1000);
+}
+
+function transitionShuttle()	{
+	var timeTillLaunch = getPickedDate();
+	clearTop();
+	document.getElementById("launch-header").style.display = "block";
+	document.body.style.backgroundImage = "url(./img/shuttle.jpg";
+	endTimeStamp = timeTillLaunch;
+	countItDown = setInterval( function() {countDownTimer(); }, 1000);
+}
+
+function countDown(sentDate2)	{
+	endTimeStamp = endTimeStamp - 1000;
+	var timeStampInSeconds = endTimeStamp / 1000;
+	var seconds = Math.floor(timeStampInSeconds % 60);
+	var minutes = Math.floor((timeStampInSeconds / 60) % 60);
+	var hours = Math.floor(timeStampInSeconds / (60 * 60) % 24);
+	var days = Math.floor(timeStampInSeconds / (60 * 60 * 24) % 7);
+	var weeks = Math.floor(timeStampInSeconds / (60 * 60 * 24 * 7));
+
+	var timeObject = {
+			weeks: weeks,
+			days: days,
+			hours: hours,
+			minutes: minutes,
+			seconds: seconds,
+			timeDifference: endTimeStamp
+	};
+	return timeObject;
+}
+
+function countDownTimer(sentTimeLeft){
+		var timeObjectReturned = countDown();
+		if(timeObjectReturned.timeDifference <= 0)	{
+			document.getElementById("countdown-timer").style.display = "none";
+			clearInterval(countItDown);
+			if(document.getElementById("trap-header").style.display === "block")	{
+				document.getElementById("trap-header").style.display = "none";
+				document.body.style.backgroundImage = "url(./img/deathstar.gif";
+			}else if(document.getElementById("lighting-header").style.display === "block") {
+				document.getElementById("lighting-header").style.display = "none";
+				document.body.style.backgroundImage = "url(./img/clocktower.gif";
+			}else if(document.getElementById("launch-header").style.display === "block") {
+				document.getElementById("launch-header").style.display = "none";
+				document.body.style.backgroundImage = "url(./img/shuttle.gif";
+			}
+		}else	{
+			weeks.innerHTML = timeObjectReturned.weeks;
+			days.innerHTML = timeObjectReturned.days;
+			hours.innerHTML = timeObjectReturned.hours;
+			minutes.innerHTML = timeObjectReturned.minutes;
+			seconds.innerHTML = timeObjectReturned.seconds;
+	}
+}
+
+function pauseTimer()	{
+	clearInterval(countItDown);
+}
+
+
+initTime();
 
